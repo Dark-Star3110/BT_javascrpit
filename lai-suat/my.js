d78530f81b8feff54dcf034c1923a218b058c0f7
Dark-Star3110 <dangvan31102001@gmail.com>
2021-05-25 20:23:38 +0700

    add

--- lai-suat/my.js
+++ lai-suat/my.js
@@ -2,6 +2,6 @@ function cal(){
     let money=+document.getElementById("money").value;
     let time=+document.getElementById("time").value;
     let rate=+document.getElementById("ir").value;
-    account=money+money*rate*time;
+    account=money+money*(rate/12)*time;
     document.getElementById("result").innerHTML="Result:"+account+" trieu VND";
 }
