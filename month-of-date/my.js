12602211a8e530082b5eef399004890489edc64c
Dark-Star3110 <dangvan31102001@gmail.com>
2021-05-27 20:26:34 +0700

    add-bt

---
+++ month-of-date/my.js
@@ -0,0 +1,33 @@
+function number_days(){
+    let month = document.getElementById("month").value;
+    let today= new Date();
+    current_year= today.getFullYear;
+    if(current_year % 4 == 0){
+        if(month==1 || month==3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            document.getElementById("result").innerHTML="Current year of month "+month+" have:"+" 31 days";
+        }
+        else if(month == 2){
+            document.getElementById("result").innerHTML="Current year of month "+month+" have:"+" 29 days";
+        }
+        else if(month== 4 || month== 6 || month == 9 || month == 11){
+            document.getElementById("result").innerHTML="Current year of month "+month+" have:"+" 30 days";
+        }
+        else{
+            document.getElementById("result").innerHTML="month is notfall!!";
+        }
+    }
+    else{
+        if(month==1 || month==3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            document.getElementById("result").innerHTML="Current year of month "+month+" have:"+" 31 days";
+        }
+        else if(month == 2){
+            document.getElementById("result").innerHTML="Current year of month "+month+" have:"+" 28 days";
+        }
+        else if(month== 4 || month== 6 || month == 9 || month == 11){
+            document.getElementById("result").innerHTML="Current year of month "+month+" have:"+" 30 days";
+        }
+        else{
+            document.getElementById("result").innerHTML="month is notfall!!";
+        }
+    }
+}
